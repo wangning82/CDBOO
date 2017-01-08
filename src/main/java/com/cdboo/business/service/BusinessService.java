82e62689a6c9e13888bf9e4e783e648cc956ea8f
mmzz <mymz@vip.qq.com>
2017-01-08 10:12:42 +0800

    纠正删除

--- src/main/java/com/cdboo/business/service/BusinessService.java
+++ src/main/java/com/cdboo/business/service/BusinessService.java
@@ -78,7 +78,7 @@ public class BusinessService extends TreeService<BusinessDao, Business> {
 	
 	@Transactional(readOnly = false)
 	public void delete(Business business) {
-		super.delete(business);
+		businessDao.delete(business);
 	}
 
 	public Page<BusinessTimestep> findTimestepByBusinessTimestep(Page<BusinessTimestep> page, BusinessTimestep businessTimestep) {
